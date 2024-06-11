cf2c842f943d8aa57fe97f29105f465fc8189deb
Sébastien Chopin <seb@nuxt.com>
2024-06-11 09:56:02 +0200

    chore: up

--- server/api/upload.post.ts
+++ server/api/upload.post.ts
@@ -1,10 +1,12 @@
 export default eventHandler(async (event) => {
+  // Make sure the user is authenticated to upload
   const { user } = await requireUserSession(event)
+
   return hubBlob().handleUpload(event, {
     types: ['image/jpeg'],
     maxSize: '1MB',
     // prefix: `${user.provider}/${user.id}`,
-    // Metatada
+    // Store user dertails in metatada
     userProvider: user.provider,
     userId: user.id,
     userName: user.name,
