c6265d3ff1db6d89c9d1938aeddd4337dcb90933
Sébastien Chopin <seb@nuxt.com>
2024-06-27 18:26:47 +0200

    feat: add infinite loading

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,14 +1,31 @@
 <script setup lang="ts">
-import { UseTimeAgo } from '@vueuse/components'
+import { UseTimeAgo, vInfiniteScroll } from '@vueuse/components'
 
-const { data: drawings } = await useFetch('/api/drawings')
+const { data } = await useFetch('/api/drawings', {
+  // don't return a shallowRef as we mutate the array
+  deep: true
+})
+
+const loading = ref(false)
+async function loadMore() {
+  if (loading.value || !data.value?.hasMore) return
+  loading.value = true
+
+  const more = await $fetch(`/api/drawings`, {
+    query: { cursor: data.value.cursor }
+  })
+  data.value.blobs.push(...more.blobs)
+  data.value.cursor = more.cursor
+  data.value.hasMore = more.hasMore
+  loading.value = false
+}
 </script>
 
 <template>
   <UPageBody>
     <UPageGrid class="lg:grid-cols-3 xl:grid-cols-4">
       <div
-        v-for="drawing in drawings"
+        v-for="(drawing, index) in data?.blobs"
         :key="drawing.pathname"
         class="flex flex-col gap-2"
       >
@@ -39,5 +56,15 @@ const { data: drawings } = await useFetch('/api/drawings')
         </div>
       </div>
     </UPageGrid>
+    <div v-if="data?.hasMore" v-infinite-scroll="[loadMore, { distance: 10, interval: 1000 }]" class="flex items-center justify-center mt-2 p-4">
+      <UButton
+        color="gray"
+        block
+        size="md"
+        :loading="loading"
+        :label="loading ? 'Loading more drawings...' : 'Load more drawings'"
+        @click="loadMore"
+      />
+    </div>
   </UPageBody>
 </template>
--- server/api/drawings.get.ts
+++ server/api/drawings.get.ts
@@ -1,7 +1,8 @@
-export default eventHandler(async () => {
-  const { blobs } = await hubBlob().list({
-    limit: 100,
-  })
+export default eventHandler(async (event) => {
+  const { cursor } = await getQuery<{ cursor?: string }>(event)
 
-  return blobs
+  return hubBlob().list({
+    limit: 100,
+    cursor
+  })
 })
