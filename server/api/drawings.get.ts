183de8bc0215103ca7712b3a819e9944dbb9be3b
Sébastien Chopin <atinux@gmail.com>
2025-12-16 20:05:54 +0100

    feat: add support from serving assets from public bucket

--- server/api/drawings.get.ts
+++ server/api/drawings.get.ts
@@ -12,19 +12,24 @@ export default eventHandler(async (event) => {
   // Fetch the custom metadata for each blob (only when not using r2 binding)
   res.blobs = await Promise.all(res.blobs.map(b => !Object.keys(b.customMetadata).length ? blob.head(b.pathname) : b))
   // backward compatibility with the old blob custom metadata
-  res.blobs = res.blobs.map(b => ({
-    ...b,
-    customMetadata: {
-      userProvider: b.customMetadata?.userProvider || b.customMetadata?.userprovider || b.customMetadata?.user_provider,
-      userId: b.customMetadata?.userId || b.customMetadata?.userid || b.customMetadata?.user_id,
-      userName: b.customMetadata?.userName || b.customMetadata?.username || b.customMetadata?.user_name,
-      userAvatar: b.customMetadata?.userAvatar || b.customMetadata?.useravatar || b.customMetadata?.user_avatar,
-      userUrl: b.customMetadata?.userUrl || b.customMetadata?.userurl || b.customMetadata?.user_url,
-      description: b.customMetadata?.description,
-      aiImage: b.customMetadata?.aiImage || b.customMetadata?.aiimage || b.customMetadata?.ai_image,
-      aiImageUrl: b.customMetadata?.aiImageUrl || b.customMetadata?.aiimageurl || b.customMetadata?.ai_image_url,
-    },
-  }))
+  // @ts-expect-error - customMetadata is not typed
+  res.blobs = res.blobs.map((b) => {
+    const aiImage = b.customMetadata?.aiImage || b.customMetadata?.aiimage || b.customMetadata?.ai_image
+    return {
+      ...b,
+      customMetadata: {
+        url: process.env.S3_PUBLIC_URL ? `${process.env.S3_PUBLIC_URL}/${b.pathname}` : undefined,
+        userProvider: b.customMetadata?.userProvider || b.customMetadata?.userprovider || b.customMetadata?.user_provider,
+        userId: b.customMetadata?.userId || b.customMetadata?.userid || b.customMetadata?.user_id,
+        userName: b.customMetadata?.userName || b.customMetadata?.username || b.customMetadata?.user_name,
+        userAvatar: b.customMetadata?.userAvatar || b.customMetadata?.useravatar || b.customMetadata?.user_avatar,
+        userUrl: b.customMetadata?.userUrl || b.customMetadata?.userurl || b.customMetadata?.user_url,
+        description: b.customMetadata?.description,
+        aiImage,
+        aiImageUrl: b.customMetadata?.aiImageUrl || (process.env.S3_PUBLIC_URL ? `${process.env.S3_PUBLIC_URL}/${aiImage}` : undefined),
+      },
+    }
+  })
 
   return res
 })
